6a10c07e3ededd2ce40adccdfc97ae0fb7e9d533
Punkbooster <2morrow2late@rambler.ru>
2016-02-16 22:52:51 +0100

    Add show, new & create methods

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -3,4 +3,28 @@ class FightersController < ApplicationController
 	def index 
 
 	end
+
+	def show
+		@fighter = Fighter.find(params[:id])
+	end
+
+	def new
+		@fighter = Fighter.new
+	end
+
+	def create 
+		@fighter = Fighter.new(fighter_params)
+
+		if @fighter.save
+			redirect_to @fighter
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def fighter_params
+		params.require(:fighter).permit(:first_name, :last_name, :description)
+	end
 end
