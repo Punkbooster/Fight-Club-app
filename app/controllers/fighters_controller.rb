f1cd6117e37b7c2994c4d1cf3db62e906673faa1
Punkbooster <2morrow2late@rambler.ru>
2016-02-16 23:05:45 +0100

    Add view files & edit/destroy methods

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -1,11 +1,11 @@
 class FightersController < ApplicationController
+	before_action :find_fighter, only: [:show, :edit, :update, :destroy]
 
 	def index 
 
 	end
 
 	def show
-		@fighter = Fighter.find(params[:id])
 	end
 
 	def new
@@ -22,7 +22,28 @@ class FightersController < ApplicationController
 		end
 	end
 
+	def edit
+
+	end
+
+	def update
+		if @fighter.update(fighter_params)
+			redirect_to @fighter
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@fighter.destroy
+		redirect_to root_path
+	end
+
 	private
+
+	def find_fighter
+		@fighter = Fighter.find(params[:id])
+	end
 
 	def fighter_params
 		params.require(:fighter).permit(:first_name, :last_name, :description)
