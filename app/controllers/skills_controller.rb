1993b72d61395c5631de7c38288a596e65c93e13
Punkbooster <2morrow2late@rambler.ru>
2016-02-25 10:53:05 +0100

    Extract private methods for skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,7 +1,8 @@
 class SkillsController < ApplicationController
+	before_action :find_fighter, only: [:create, :edit, :update, :destroy]
+	before_action :find_skill, only: [:edit, :update, :destroy]
 
 	def create
-		@fighter = Fighter.find(params[:fighter_id])
 		@skill = @fighter.skills.create(params[:skill].permit(:name, :level))
 
 		if @skill.save
@@ -12,26 +13,31 @@ class SkillsController < ApplicationController
 	end
 
 	def edit 
-		@fighter = Fighter.find(params[:fighter_id])
-		@skill = @fighter.skills.find(params[:id])
+
 	end
 
 	def update
-		@fighter = Fighter.find(params[:fighter_id])
-		@skill = @fighter.skills.find(params[:id])
-
 		if @skill.update(params[:skill].permit(:name, :level))
 			redirect_to fighter_path(@fighter)
 		else
 			render 'edit'
 		end
+
 	end	
 
 	def destroy
-		@fighter = Fighter.find(params[:fighter_id])
-		@skill = @fighter.skills.find(params[:id])
 		@skill.destroy
 		redirect_to fighter_path(@fighter)
+	end
+
+private
+	
+	def find_fighter
+		@fighter = Fighter.find(params[:fighter_id])
+	end
+
+	def find_skill
+		@skill = @fighter.skills.find(params[:id])
 	end
 
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,6 @@
 class Skill < ActiveRecord::Base
   belongs_to :fighter
 
-  validates_numericality_of :level, greater_than: 0, less_than_or_equal_to: 5
+  #validates_numericality_of :level, greater_than: 0, less_than_or_equal_to: 5
 
 end
