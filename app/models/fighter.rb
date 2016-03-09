2300656202cac31e19021ad56c25903115ade7a4
Punkbooster <2morrow2late@rambler.ru>
2016-03-09 14:24:49 +0100

    AWS issue #3

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -2,8 +2,7 @@ class Fighter < ActiveRecord::Base
 	has_many :skills
 
 	has_attached_file :image, 
-		styles: { medium: "300x300>", thumb: "100x100>" },
-
+		styles: { medium: "300x300>", thumb: "100x100>" }
 		validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 		validates_presence_of :image
 		
